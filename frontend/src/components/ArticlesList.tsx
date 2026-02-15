9de79e7e18b0b33d704ca67d1519f650fe9fc40b
janek <jan.skiba.poczta@gmail.com>
2026-02-15 22:37:12 +0100

    fix: remove redundant class from ArticlesListPage container

--- frontend/src/components/ArticlesList.tsx
+++ frontend/src/components/ArticlesList.tsx
@@ -53,7 +53,7 @@ export function ArticlesListPage() {
   }
 
   return (
-    <div className="min-h-screen  py-12 px-4 sm:px-6 lg:px-8">
+    <div className="py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-4xl mx-auto">
         {/* Search Input */}
         <div className="relative mb-8 group">
