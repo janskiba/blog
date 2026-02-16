96feac7aa5c180740272e20b3d2d363ac211961c
janek <jan.skiba.poczta@gmail.com>
2026-02-16 01:16:21 +0100

    fix: adjust padding in Menu component for improved layout consistency

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Menu } from "./Menu"
 export function Layout() {
   return (
     <div className="min-h-screen bg-gradient-to-br  from-gray-900 via-gray-800 to-gray-900">
-      <header className="sticky h-20 top-0 backdrop-blur-md border-b border-b-slate-600 z-20">
+      <header className="sticky top-0 backdrop-blur-md border-b border-b-slate-600 z-20">
         <div className=" mx-auto  ">
           <Menu />
         </div>
