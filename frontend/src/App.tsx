87d25e7a9ba7c1e2c218ce1ba13becd90226d08a
janek <jan.skiba.poczta@gmail.com>
2026-02-16 19:49:13 +0100

    fix: adjust layout in App component for improved responsiveness and remove unused CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,9 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="min-w-screen min-h-screen bg-linear-to-br  from-gray-900 via-gray-800 to-gray-900">
+    <div className="w-full min-h-screen bg-linear-to-br  from-gray-900 via-gray-800 to-gray-900">
       <header className="sticky top-0 backdrop-blur-md border-b border-b-slate-600 z-20">
-        <div className=" mx-auto  ">
           <Menu />
-        </div>
       </header>
       <main className="mx-auto max-w-3xl ">
         <Outlet />
