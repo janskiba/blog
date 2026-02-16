e211dbe0a86f7bd36c8b345bbaf8f816912b85d0
janek <jan.skiba.poczta@gmail.com>
2026-02-16 22:37:22 +0100

    fix: update header styling in App component for improved visual clarity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="w-full min-h-screen bg-linear-to-br  from-gray-900 via-gray-800 to-gray-900">
-      <header className="sticky top-0 backdrop-blur-md border-b border-b-slate-600 z-20">
+      <header className="sticky bg-black/10 top-0 backdrop-blur-md z-20">
           <Menu />
       </header>
       <main className="mx-auto max-w-3xl ">
