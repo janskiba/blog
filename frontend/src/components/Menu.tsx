0e3cdde430ea0b252618375a3c5526a067a3e39a
janek <jan.skiba.poczta@gmail.com>
2026-02-16 19:20:49 +0100

    fix: improve styling and layout in Menu component for better visual consistency

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -10,8 +10,8 @@ export function Menu() {
 
     return (
         <div className="flex items-center justify-between px-5 py-3 ">
-            <div className="inline-flex items-center rounded-2xl border border-gray-700/50 bg-linear-to-br from-gray-800/50 to-gray-900/50 px-3 py-1 backdrop-blur-sm">
-                <span className="pointer-events-none absolute inset-0 rounded-2xl bg-linear-to-r from-blue-500/5 via-purple-500/5 to-pink-500/5 transition-all duration-300" />
+            <div className="inline-flex items-center rounded-2xl border border-white/10 bg-gray-900/20 px-4 py-2 backdrop-blur-md shadow-lg">
+                <span className="pointer-events-none absolute inset-0 rounded-2xl bg-linear-to-r from-blue-500/10 via-purple-500/10 to-pink-500/10 transition-all duration-300" />
                 <span className="relative pointer-events-none text-lg font-bold text-gray-100">
                     janskiba.dev
                 </span>
@@ -27,12 +27,12 @@ export function Menu() {
                                 end
                                 className={({ isActive, isPending }) =>
                                     [
-                                        "inline-flex items-center rounded-xl px-4 py-2 text-sm font-medium",
-                                        "bg-gray-800/40 backdrop-blur-sm border border-gray-700/50",
-                                        "text-gray-200! visited:text-gray-200 transition-all duration-200",
-                                        "hover:bg-gray-800/70 hover:border-blue-500/50 hover:text-blue-200 visited:hover:text-blue-200",
+                                        "inline-flex items-center rounded-xl px-4 py-2 text-sm font-medium transition-all duration-100",
+                                        "bg-gray-800/30 backdrop-blur-md border border-white/10 shadow-sm",
+                                        "text-gray-200! visited:text-gray-200",
+                                        " hover:border-blue-500/30 hover:text-blue-200 visited:hover:text-blue-200 hover:shadow-blue-500/20 hover:shadow-md",
                                         "focus:outline-none focus:ring-2 focus:ring-blue-500/50",
-                                        isActive ? "bg-blue-500/10 border-blue-500/60 text-blue-200 visited:text-blue-200" : "",
+                                        isActive ? "bg-white/10 border-blue-500/50 text-blue-100 visited:text-blue-100 shadow-blue-500/20" : "",
                                         isPending ? "opacity-70" : "",
                                     ].join(" ")
                                 }
