7a4e781f18eba1c1bb2c41330d683e9f97015004
Jan Skiba <jan.skiba.poczta@proton.me>
2026-02-22 23:43:54 +0100

    fix: update PUBLIC_URL in server configuration to use the correct CMS URL

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
 
-  url: env('PUBLIC_URL', 'https://janskiba.dev'),
+  url: env('PUBLIC_URL', 'https://cms.janskiba.dev'),
 
   app: {
     keys: env.array('APP_KEYS'),
