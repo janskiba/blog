18eb008fbbfed446152315069b228655fd1b8406
Jan Skiba <jan.skiba.poczta@proton.me>
2026-02-23 22:08:01 +0100

    fix: update CORS middleware configuration for improved security and flexibility

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -4,7 +4,16 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://janskiba.dev', 'http://localhost:5173'],
+      methods: ['GET', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
